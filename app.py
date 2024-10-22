51bbaad8d6524058e15351c5b4013aa724878f8a
CarlosR2104 <carlosmtz2g3@gmail.com>
2024-10-21 16:00:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import mysql.connector
 import datetime
 import pytz
@@ -18,7 +18,7 @@ pusher_client = pusher.Pusher(
     key='25ef14f3e9a47c712e61',
     secret='31c5ddebc75a9419e7d2',
     cluster='us2',
-     ssl=True
+    ssl=True
 )
 
 app = Flask(__name__)
@@ -26,10 +26,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("app.html")
-
-@app.route("/alumnos")
-def alumnos():
-    return render_template("alumnos.html")
 
 @app.route("/reservas/guardar", methods=["POST"])
 def reservasGuardar():
@@ -45,7 +41,7 @@ def reservasGuardar():
     val = (nombre_apellido, telefono, fecha)
     cursor.execute(sql, val)
     con.commit()
-    
+
     # Emitir un evento de Pusher
     pusher_client.trigger('reservas-channel', 'nueva-reserva', {
         'nombre_apellido': nombre_apellido,
@@ -55,7 +51,7 @@ def reservasGuardar():
 
     con.close()
 
-    return f"Reserva guardada: Nombre y Apellido {nombre_apellido}, Teléfono {telefono}, Fecha {fecha}"
+    return f"Reserva guardada: {nombre_apellido}, Teléfono: {telefono}, Fecha: {fecha}"
 
 @app.route("/reservas/buscar")
 def reservasBuscar():
@@ -68,7 +64,43 @@ def reservasBuscar():
 
     con.close()
 
-    return registros
+    return jsonify(registros)
+
+@app.route("/reservas/actualizar", methods=["POST"])
+def reservasActualizar():
+    id_reserva = request.form["txtIdReserva"]
+    nombre_apellido = request.form["txtNombreApellido"]
+    telefono = request.form["txtTelefono"]
+
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor()
+    sql = "UPDATE tst0_reservas SET Nombre_Apellido = %s, Telefono = %s WHERE Id_Reserva = %s"
+    val = (nombre_apellido, telefono, id_reserva)
+    cursor.execute(sql, val)
+    con.commit()
+
+    con.close()
+
+    return f"Reserva actualizada: {nombre_apellido}, Teléfono: {telefono}"
+
+@app.route("/reservas/eliminar", methods=["POST"])
+def reservasEliminar():
+    id_reserva = request.form["id_reserva"]
+
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor()
+    sql = "DELETE FROM tst0_reservas WHERE Id_Reserva = %s"
+    val = (id_reserva,)
+    cursor.execute(sql, val)
+    con.commit()
+
+    con.close()
+
+    return "Reserva eliminada correctamente"
 
 if __name__ == "__main__":
     app.run(debug=True)
